4c1848792a032a380fec10966c7fb92a19d2cc72
marcellop <marcello.palmitessa@gmail.com>
2019-03-01 07:31:06 +0100

    wip

---
+++ components/MoviesScrollableList.vue
@@ -0,0 +1,35 @@
+<template>
+  <section class="movie-list movie-list--scrollable">
+    <header>
+      <h2>{{ title }}</h2>
+    </header>
+    <div class="movie-list__container">
+      <MovieCard v-for="(movie, index) in movies" :key="index"
+                 :movie="movie"
+      >
+      </MovieCard>
+    </div>
+  </section>
+</template>
+
+<script>
+import MovieCard from './MovieCard';
+
+export default {
+  name: 'MoviesListScrollable',
+  components: { MovieCard },
+  props: {
+    title: {
+      required: true,
+      type: String
+    },
+    movies: {
+      required: true,
+      type: Array,
+      default: function () {
+        return [];
+      }
+    }
+  }
+};
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -7,7 +7,7 @@
       <h2>Top Rated</h2>
     </section>
 
-    <MoviesList :movies="topRated"></MoviesList>
+    <MoviesListScrollable title="Top rated" :movies="topRated"></MoviesListScrollable>
   </main>
 </template>
 
@@ -15,12 +15,14 @@
 import MoviesList from '../components/MoviesList';
 // import LatestMovie from '../components/LatestMovie';
 import Upcoming from '../components/Upcoming';
+import MoviesListScrollable from '../components/MoviesScrollableList';
 
 export default {
   components: {
-    Upcoming,
+    MoviesListScrollable,
+    Upcoming
     // LatestMovie,
-    MoviesList,
+    // MoviesList,
   },
 
   computed: {
