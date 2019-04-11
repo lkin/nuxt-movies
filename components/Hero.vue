423c346cf85d4231dfb990b2d83dd49d8eb86950
marcellop <marcello.palmitessa@gmail.com>
2019-04-11 21:24:03 +0200

    added security header wip func comps

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,47 +1,45 @@
 <template>
-  <no-ssr>
-    <transition name="fade" mode="out-in">
+  <transition name="fade" mode="out-in">
 
-      <article v-if="medium.id !== undefined"
-               class="hero"
-            >
-            <figure>
-              <v-lazy-image v-if="pictureExist"
-                            sizes="(max-width: 1280px) 100vw, 1280px"
-                            :srcset="`${backdropPath(medium, 'w300')} 320w,${backdropPath(medium, 'w780')} 768w,${backdropPath(medium, 'w1280')} 1280w`"
-                            :src="backdropPath(medium, 'w1280')"
-              />
-              <img v-else
-                   :src="placeholder"
-                   alt=""
-              >
-              <figcaption>
-                <span class="hero__tag">Movies</span>
-                <h1>
-                  <nuxt-link :to="`/media/${medium.id}`">
-                    {{ medium.title }}
-                  </nuxt-link>
-                </h1>
-                <p class="hero__genres"><span v-for="id in medium.genre_ids" :key="id">{{ getGenre(id) }}</span></p>
-                <p class="hero__info-small">{{ formatDate(medium.release_date) }}</p>
-                <p class="hero__info-small">Popularity: {{ medium.vote_average * 10 }}%</p>
-                <p class="hero__abstract">{{ getAbstract(medium) }}</p>
-                <button class="button button--icon">
-                  Play trailer
-                  <span class="button__icon" aria-hidden="true">
+    <article v-if="medium.id !== undefined"
+             class="hero"
+    >
+      <figure>
+        <v-lazy-image v-if="pictureExist"
+                      sizes="(max-width: 1280px) 100vw, 1280px"
+                      :srcset="`${backdropPath(medium, 'w300')} 320w,${backdropPath(medium, 'w780')} 768w,${backdropPath(medium, 'w1280')} 1280w`"
+                      :src="backdropPath(medium, 'w1280')"
+        />
+        <img v-else
+             :src="placeholder"
+             alt=""
+        >
+        <figcaption>
+          <span class="hero__tag">Movies</span>
+          <h1>
+            <nuxt-link :to="`/media/${medium.id}`">
+              {{ medium.title }}
+            </nuxt-link>
+          </h1>
+          <p class="hero__genres"><span v-for="id in medium.genre_ids" :key="id">{{ getGenre(id) }}</span></p>
+          <p class="hero__info-small">{{ formatDate(medium.release_date) }}</p>
+          <p class="hero__info-small">Popularity: {{ medium.vote_average * 10 }}%</p>
+          <p class="hero__abstract">{{ getAbstract(medium) }}</p>
+          <button class="button button--icon">
+            Play trailer
+            <span class="button__icon" aria-hidden="true">
                     <svg>
                       <use xlink:href="#icon-film-solid"></use>
                     </svg>
                   </span>
-                </button>
-              </figcaption>
-            </figure>
-          </article>
-      <section v-else>
-        <FilmStripLoader></FilmStripLoader>
-      </section>
-    </transition>
-  </no-ssr>
+          </button>
+        </figcaption>
+      </figure>
+    </article>
+    <section v-else>
+      <FilmStripLoader></FilmStripLoader>
+    </section>
+  </transition>
 </template>
 
 <script>
@@ -71,9 +69,9 @@ export default {
       return d.format('MMM D, YYYY');
     },
 
-    getAbstract(medium){
+    getAbstract(medium) {
       const abstract = medium.tagline || medium.overview;
-      return abstract.length > 160 ? (abstract.slice(0,160) + '...') : abstract;
+      return abstract.length > 160 ? (abstract.slice(0, 160) + '...') : abstract;
     },
 
     getGenre(id) {
