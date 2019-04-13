a20730c763488fae8607d7c3cdae3c1b116f5180
marcellop <marcello.palmitessa@gmail.com>
2019-04-13 18:39:48 +0200

    fix

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,19 +1,18 @@
 const path = require('path');
 
-module.exports = {
-  // // allow to use @ to reference components, i.e. import List from '@/components/list/List'
-  // resolve: {
-  //   alias: {
-  //     '@': path.dirname(path.resolve(__dirname))
-  //   }
-  // },
-  module: {
-    rules: [
-      {
-        test: /\.scss$/,
-        loaders: ['style-loader', 'css-loader', 'sass-loader'],
-        // include: path.resolve(__dirname, '../'),
-      },
-    ],
-  },
+// Export a function. Accept the base config as the only param.
+module.exports = async ({ config, mode }) => {
+  // `mode` has a value of 'DEVELOPMENT' or 'PRODUCTION'
+  // You can change the configuration based on that.
+  // 'PRODUCTION' is used when building the static version of storybook.
+
+  // Make whatever fine-grained changes you need
+  config.module.rules.push({
+    test: /\.scss$/,
+    loaders: ['style-loader', 'css-loader', 'sass-loader'],
+    // include: path.resolve(__dirname, '../'),
+  });
+
+  // Return the altered config
+  return config;
 };
